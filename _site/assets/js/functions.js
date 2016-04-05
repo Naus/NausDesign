bf66b0ef38d45033a459a6f524e0720079b63b22
Chris Naus <NausDesign@gmail.com>
2016-04-05 17:10:44 -0500

    added menu slide out
    added pop out menu for site nav, updated html in header for  nav,
    updated JS for nav

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -1,4 +1,10 @@
-$(window).scroll(function(){
-  var wScroll = $(this).scrollTop();
+// Menu pop out for site nave
 
+$(document).ready(function(){
+  $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+    $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
 });
+
+//---------------------------------------------
